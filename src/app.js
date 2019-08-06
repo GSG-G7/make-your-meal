78806469ae55eddd200dafb2ed6223ffa443f8fe
FadiAlamassi <alamassifadi@gmail.com>
2019-08-06 15:02:14 +0300

    add views to the server and create the engine Relates #4

--- src/app.js
+++ src/app.js
@@ -8,7 +8,18 @@ const app = express();
 
 const routes = require('./routes');
 // helper
-//// you need to access with hbs files 
+// // you need to access with hbs files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+  }),
+);
 
 const port = process.env.PORT || 3000;
 
