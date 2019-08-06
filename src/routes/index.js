e9f253ec9658ad10e190eb06a9f293e958e1d7ad
fadi omar <fadeomar2015@gmail.com>
2019-08-06 16:35:51 +0300

    fetch data form server and api and put this data into home.hbs Relates #6

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+const search = require('./search');
+
+const fetch = require('node-fetch');
+
+const Bluebird = require('bluebird');
+
+fetch.Promise = Bluebird;
 
 const router = express.Router();
 
@@ -7,4 +14,13 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
+router.post('/search', (req, res) => {
+  const query = 'p';
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  fetch(url)
+    .then(data => data.json())
+    .then(data => res.render('home', {
+      meals: data.meals,
+    }));
+});
 module.exports = router;
